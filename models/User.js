cf89d384a58d0a8bf716ad85a826f13c7bd16ab3
dlesesne23 <152806585+dlesesne23@users.noreply.github.com>
2024-06-14 01:11:51 -0500

    Attempting again

--- models/User.js
+++ models/User.js
@@ -6,10 +6,6 @@ const userSchema = new mongoose.Schema({
     username: {type: String, required: true, unique: true},
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    location: {
-        type: { type: String, default: 'Point' },
-        coordinates: [Number],
-      },
 }, {
     timestamps: true,
     toJSON: {
